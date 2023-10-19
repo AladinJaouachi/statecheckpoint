1b5d750652fd3258c566480359fb9a20c8138f4a
Aladin-Jaouachi <alaajawachi5@gmail.com>
2023-10-19 15:36:06 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -14,13 +14,31 @@ export default class App extends Component {
           "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1icwxY.img",
         profession: "joueur de football"
       },
-      bool: true
+      bool: true,
+      mountTime: new Date(),
     };
     this.updateState = this.updateState.bind(this);
   }
   updateState() {
     this.setState({ bool: !this.state.bool });
+  };
+  calculateMountTime = () => {
+    const currentTime = new Date();
+    const elapsedTime = Math.floor((currentTime - this.state.mountTime) / 1000); // Calculate time in seconds
+    return elapsedTime;
+  };
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.forceUpdate(); // Force a re-render to update the time
+    }, 1000); // Update time every second
   }
+  componentWillUnmount() {
+    clearInterval(this.interval); // Clear the interval when the component is unmounted
+  }
+
+
+
+
 
   render() {
     return (
@@ -38,7 +56,7 @@ export default class App extends Component {
         <Button variant="primary" onClick={()=> this.updateState()} > {this.state.bool ? "hide" : "show"} </Button>
         
      
-        
+        <p>Time since mount: {this.calculateMountTime()} seconds</p> 
       </>
     );
   }
